da838daaefe22d4fa9bc9fe8230e4649d44e7453
Federico <146098601+FeDeveloper95@users.noreply.github.com>
2024-12-15 14:43:38 +0100

    Major update to GSI Flash Tool.bat

--- GSI Flash Tool.bat
+++ GSI Flash Tool.bat
@@ -1,10 +1,62 @@
 @echo off
 
-rem Verifica che adb sia installato
-where adb >nul 2>&1 || (
-  echo "adb non è installato. Installalo prima di eseguire questo script."
-  exit /b 1
-)
+echo Welcome to GSI Flash Tool by FeDeveloper95 - https://github.com/FeDeveloper95
+pause
 
-rem Riavvia il dispositivo in modalità recovery
-color a && fastboot reboot fastboot && fastboot erase userdata && fastboot erase metadata && fastboot delete-logical-partition product_b && fastboot delete-logical-partition product && fastboot delete-logical-partition product_a && fastboot --disable-verity --disable-verification flash vbmeta vbmeta.img && fastboot flash vbmeta vbmeta.img && fastboot flash system system.img && fastboot flash recovery recovery.img && fastboot erase userdata && fastboot erase metadata && fastboot reboot
+:inizio
+
+echo.
+echo Choose the current state of the device:
+echo   1. Booted in AndroidOS
+echo   2. Recovery
+echo   3. Bootloader
+echo   4. Fastboot
+echo   5. Quit
+set /p "scelta=Input: "
+
+rem Esci se l'utente sceglie 5
+if "%scelta%"=="5" exit /b 0
+
+rem Esegui l'azione in base alla scelta dell'utente
+if "%scelta%"=="4" goto fastboot
+if "%scelta%"=="3" goto bootloader
+if "%scelta%"=="2" goto recovery
+if "%scelta%"=="1" goto avviato
+goto inizio  
+
+:avviato
+rem Riavvia in fastboot se il dispositivo è avviato normalmente
+adb reboot bootloader
+goto flashing
+
+:recovery
+rem Riavvia in fastboot se il dispositivo è in recovery
+adb reboot bootloader
+goto flashing
+
+:bootloader
+rem Riavvia in fastboot se il dispositivo è in bootloader
+fastboot reboot fastboot
+goto flashing
+
+:fastboot
+rem Non riavviare se il dispositivo è già in fastboot
+
+:flashing
+rem Esegui le operazioni di flashing
+fastboot erase userdata
+fastboot erase metadata
+fastboot delete-logical-partition product_b
+fastboot delete-logical-partition product
+fastboot delete-logical-partition product_a
+fastboot --disable-verity --disable-verification flash vbmeta vbmeta.img
+fastboot flash vbmeta vbmeta.img
+fastboot flash system system.img
+fastboot flash recovery recovery.img
+fastboot erase userdata
+fastboot erase metadata
+fastboot reboot
+
+echo Flashed!
+
+rem Done
